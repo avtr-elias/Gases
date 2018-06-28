fef88d961184b35132e2d80edc9159f0fce28412
Нурым Кенжебеков <nkenzhebekov@ecoservice.kz>
2018-06-28 08:20:44 +0600

    Upload

--- Gases/Controllers/UploadController.cs
+++ Gases/Controllers/UploadController.cs
@@ -13,35 +13,38 @@ namespace Gases.Controllers
 {
     public class UploadController : Controller
     {
-        ApplicationContext _context;
+        //ApplicationContext _context;
         IHostingEnvironment _appEnvironment;
 
-        public UploadController(ApplicationContext context, IHostingEnvironment appEnvironment)
+        public UploadController(/*ApplicationContext context, */IHostingEnvironment appEnvironment)
         {
-            _context = context;
+            //_context = context;
             _appEnvironment = appEnvironment;
         }
 
         public IActionResult Index()
         {
-            return View(_context.Files.ToList());
+            return View(/*_context.Files.ToList()*/);
             //return View();
         }
         [HttpPost]
+
         public async Task<IActionResult> AddFile(IFormFile uploadedFile)
         {
             if (uploadedFile != null)
             {
                 // путь к папке Files
-                string path = "/Uploaded/" + uploadedFile.FileName;
+                //string path = "/Uploaded/" + uploadedFile.FileName;
+                string path = Path.Combine(_appEnvironment.WebRootPath, "Uploaded", Path.GetFileName(uploadedFile.FileName));
                 // сохраняем файл в папку Files в каталоге wwwroot
-                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                //using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                using (var fileStream = new FileStream(path, FileMode.Create))
                 {
                     await uploadedFile.CopyToAsync(fileStream);
                 }
                 UploadModel file = new UploadModel { Name = uploadedFile.FileName, Path = path };
-                _context.Files.Add(file);
-                _context.SaveChanges();
+                //_context.Files.Add(file);
+                //_context.SaveChanges();
             }
 
             return RedirectToAction("Index");
